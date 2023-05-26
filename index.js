8753e6253b6669ca62e77dfb884567add2071173
Dustin Bonilla <bonilla.dustin@gmail.com>
2023-05-25 23:51:50 -0700

    migrated utils from starter code and added gitIgnore, updated

--- index.js
+++ index.js
@@ -72,10 +72,25 @@
 // #endregion
 
 
-//expecting user to cmd line <- node index.js
-
 //#region Requirements
 const fs = require('fs');
 const inq = require('inquirer');
 //#endregion
 
+
+//#region Starter Code
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+//#endregion
