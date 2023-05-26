6cc70318f1007e693eba37d3f2c84748f5b37a3e
Dustin Bonilla <bonilla.dustin@gmail.com>
2023-05-25 18:14:23 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,70 @@
+// #region METACODE
+// Author: Dustin Bonilla
+// Project Name: README Generator
+
+/*      USER STORY
+ *_______________________________________________________________________________
+ * :: DEVELOPER
+ *  I WANT a README Generator
+ *  SO THAT I can quickly create a professional README for a new project
+*/
+
+/*      ACCEPTANCE CRITERIA
+ *_______________________________________________________________________________
+ * .: a CMD-Line app that accepts user input
+ * 
+ * @ prompted for info about my app repo
+ *  >   a HQ, prof README.md is generated w/:
+ *    > myProject.Title
+ *    > sxns {
+ *          Descriptions
+ *          Table of Contents
+ *          Installation
+ *          Usage
+ *          License
+ *          Contributing
+ *          Tests
+ *          Questions
+ *      }
+ * 
+ * @ enter my project title
+ *  > this is displayed as the title of the README
+ * 
+ * @ enter sxn info
+ *  > INFO added to the appropriate sxns
+ * 
+ * @ choose lic for app fr: a LIST ( opts )
+ *  > BADGE for that lic added to TOP of README
+ *  > NOTICE is added to the sxn("LIC"), explaining which lic the app is under
+ * 
+ * @ enter my GH username
+ *  > INFO added to sxn( "?'s" ) w/
+ *      > links to GH profile
+ * 
+ * @ enter my email addr
+ *  > ADDR added to sxn( "?'s" ) w/
+ *      > instructions on how to reach me with additional questions
+ * 
+ * @ clicks links in sxn( ToC )
+ *  > taken to corresp sxn
+ * 
+*/
+
+/*      OTHER NOTES
+ *_______________________________________________________________________________
+ *  //TODO: NEED TO SCREENCASTIFY TUTORIAL
+ * 
+ *  need to ignore node module + ds store !BEFORE install dependencies
+ *  gonna require file sys
+ *  gonna require inquirer
+ *  need to init npm -> package created
+ *  need to install inq -> package lock
+ *  
+ *  use prompt to recieve input
+ *  Output to a readme.md
+ * 
+ * 
+*/
+// #endregion
+
+
